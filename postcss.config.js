ffb5b0f9d812e53d14398b4cc7fa65cc886c7940
Sammed9665 <chougulesammed41@gmail.com>
2025-04-02 10:40:47 +0530

    job portal

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// Example postcss.config.js
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    // Other PostCSS plugins if any
+  ]
+}
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    At this point everything works perfectly
+    <p>This Fonts
+    </p>
+    </div>
+  )
+}
+
+export default App
